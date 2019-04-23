bf1e825ae3cd7ee55c79d9f0b8a2bf7d5167b7fc
tlc2point0 <lsystechinstructor@gmail.com>
2019-04-23 15:06:57 -0700

    Starting DEATH function - midday commit

--- jsProject1/app.js
+++ jsProject1/app.js
@@ -1,8 +1,8 @@
 // JavaScript Project 1
+const gameTitle = 'Lunarlvst: The Bloodmoon'
 
 
-//Daniel has edited this file. 
-//I LIKE BIG BUTTS AND I CANNOT LIE
+//Daniel has edited this file.
 
 // I'm editing this file - Jesse Dekel
 
@@ -26,7 +26,7 @@ function c() {
 
 
 // Define current state
-let States = {currentState: 'start', possibleStates: ['start', 'deathByHunger', 'lunarBase', 'vampireRoom', 'deathByVampire', 'turnedIntoVampire', 'escortedOutByVampire']}
+let States = {currentState: 'start', possibleStates: ['start', 'steelDoor', 'lastBoss', 'vampireRoom', 'DEATH']}
 
 // Inventory and status info
 let inventory = {
@@ -46,7 +46,7 @@ let playerStatus = {
 // Start the game
 function start() {
   States.currentState = 'start';
-  c('Welcome to the Tech Learning Center 2.0 Console Adventure Game!',
+  c(`Welcome to ${gameTitle}, a Tech Learning Center 2.0 Console Adventure Game!`,
   '\n================',
   '\nFollow instructions and pay attention to the game to win.',
   "\n================");
@@ -56,26 +56,24 @@ function start() {
   c('You can [look] around, but you can\'t move yet.');
 }
 
+function steelDoor() {
+  // Add the steelDoor state as a possible state in the States list above
+  // Change the States.currentState variable to be this state
+  States.currentState = 'steelDoor';
+  // Do steelDoor state stuff (i.e. DIE)
+  DEATH();
+}
+
 // Make a look function that tells the player what they see depending on the state they're in
 function _look() {
   switch (States.currentState) {
     case 'start':
       // describe stuff in the start room
-      c('Observing the table, you see a pair of keys just within reach of your feet. You can [take] the keys if you like.');
-      break;
-    case 'deathByHunger':
-      // describe stuff in the death by hunger state
-      break;
-    case 'lunarBase':
+      c('Observing the table, you see a pair of keys just within reach of your feet. You can [take] the keys if you like, or [wait].');
       break;
     case 'vampireRoom':
       break;
-    case 'deathByVampire':
-      break;
-    case 'turnedIntoVampire':
-      break;
-    case 'escortedOutByVampire':
-      break;
+
   };
 };
 
@@ -87,20 +85,10 @@ function _take() {
         c('You cannot take the key since you already have it.');
       } else {
         inventory.hasStartKey = true;
-        c('You took the key. Now you can [use] the key or wait.')
+        c('You took the key. Now you can [use] the key or [wait].')
       }
       break;
-    case 'deathByHunger':
-      break;
-    case 'lunarBase':
-      break;
     case 'vampireRoom':
-      break;
-    case 'deathByVampire':
-      break;
-    case 'turnedIntoVampire':
-      break;
-    case 'escortedOutByVampire':
       break;
   }
 };
@@ -114,43 +102,36 @@ function _use() {
     case 'start':
       c('You use the key and are now out of the cage!', '\nYou can now exit through a [broken] wood door or the [steel] door');
       break;
-    case 'deathByHunger':
-      break;
-    case 'lunarBase':
-      break;
     case 'vampireRoom':
-      break;
-    case 'deathByVampire':
-      break;
-    case 'turnedIntoVampire':
-      break;
-    case 'escortedOutByVampire':
       break;
   }
 };
 
 // Function for the vampireRoom state
 function vampireRoom() {
+  // REMINDER: Add the name of the state/function to the list of possible states above
   // CHANGE STATE VARIABLE TO vampireRoom!
   States.currentState = 'vampireRoom';
   // Do vampire room stuff
   c("There's a vampire in this room! She's extra friendly and has geeky glasses. You think she is cute.");
 };
 
-function lunarBase() {
-  // Change state variable to lunarBase
-  States.currentState = 'lunarBase';
-  // Do lunar base stuff
+function DEATH() {
+  // Edit the list of possible states to include DEATH
+  // Change current state variable to DEATH
+  States.currentState = 'DEATH';
+  // Do death stuff depending on the state
+  switch (States.currentState) {
+    case 'start':
+      c();
+      start();
+      break;
+    case 'steelDoor':
+      c();
+      start();
+      break;
+  }
 }
-
-
-// Function for escorted out state
-
-// function for death by vampire state
-
-// function for death by hunger state
-
-// 
 
 
 // Look function command
@@ -165,7 +146,8 @@ Object.defineProperties(window, {
   'check': {get: _checkStatus},
   'use': {get: _use},
   'broken': {get: vampireRoom},
-  'steel': {get: lunarBase}
+  'steel': {get: steelDoor},
+  'wait': {get: DEATH}
 });
 
 start();
