07801acc458584b21b16498ed80b85788381f1e4
tlc2point0 <lsystechinstructor@gmail.com>
2019-05-14 15:43:47 -0700

    Last version of TLC 2.0 Files

---
+++ jsProject2/presentation workspace/app.js
@@ -0,0 +1,34 @@
+console.log("hi");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Project {
+//   constructor(title, topic, time) {
+//     this.title = title;
+//     this.topic = topic;
+//     this.time = time;
+//     // this.name = name;
+//     // this.sound = sound;
+//   }
+
+//   present(title, topic, time){
+//     console.log(title + topic + "will take" + time);
+//   }
+// }
+
+// let es6 = new Project('Parker and Zabdiel\'s Project', 'ES6', 'at least 4');
+
+// es6.present();
